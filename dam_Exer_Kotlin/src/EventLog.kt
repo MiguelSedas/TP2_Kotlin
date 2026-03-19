9c6257e5c8da613b307914375014ad98c1ece917
Miguel Sedas <miguelsedas04@gmail.com>
2026-03-19 16:37:32 +0000

    Exercise 2 - Done

--- dam_Exer_Kotlin/src/EventLog.kt
+++ dam_Exer_Kotlin/src/EventLog.kt
@@ -2,4 +2,15 @@ sealed class Event {
     data class Login(val username: String, val timestamp: Long) : Event()
     data class Purchase(val username: String, val amount: Double, val timestamp: Long) : Event()
     data class Logout(val username: String, val timestamp: Long) : Event()
+
+    fun List<Event>.filterByUser(username: String): List<Event> {
+        val eventsByUser = filter { it ->
+            when (it) {
+                is Event.Login    -> it.username == username
+                is Event.Purchase -> it.username == username
+                is Event.Logout   -> it.username == username
+            }
+        }
+        return eventsByUser
+    }
 }
