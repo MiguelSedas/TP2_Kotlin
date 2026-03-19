7ed8abbee4bf480d48fbac684235e8fe6e80c30c
Miguel Sedas <miguelsedas04@gmail.com>
2026-03-19 16:34:58 +0000

    Exercise 1 - Done

---
+++ dam_Exer_Kotlin/src/EventLog.kt
@@ -0,0 +1,5 @@
+sealed class Event {
+    data class Login(val username: String, val timestamp: Long) : Event()
+    data class Purchase(val username: String, val amount: Double, val timestamp: Long) : Event()
+    data class Logout(val username: String, val timestamp: Long) : Event()
+}
