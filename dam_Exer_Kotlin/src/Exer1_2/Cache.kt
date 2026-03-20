afb15134ca89b9af3163b809c25550d81b1057be
Miguel Sedas <miguelsedas04@gmail.com>
2026-03-20 17:56:13 +0000

    Exercise 1 - Done

---
+++ dam_Exer_Kotlin/src/Exer1_2/Cache.kt
@@ -0,0 +1,22 @@
+package Exer1_2
+
+class Cache<K : Any, V : Any> {
+    private val map = mutableMapOf<K, V>()
+
+    fun put(key: K, value: V) {
+        map[key] = value
+    }
+
+    fun get(key: K): V? {
+        return map[key]
+    }
+
+    fun evict(key: K) {
+        map.remove(key)
+    }
+
+    fun size(): Int {
+        return map.size
+    }
+
+}
