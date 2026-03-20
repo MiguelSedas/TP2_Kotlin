b5368dbd4c2fdfa9f774ea566e029c4ba51405bb
Miguel Sedas <miguelsedas04@gmail.com>
2026-03-20 08:38:16 +0000

    Exercise 5 - Done

--- dam_Exer_Kotlin/src/EventLog.kt
+++ dam_Exer_Kotlin/src/EventLog.kt
@@ -1,8 +1,12 @@
+import Event.Login
+import Event.Logout
+import Event.Purchase
+
 sealed class Event {
     data class Login(val username: String, val timestamp: Long) : Event()
     data class Purchase(val username: String, val amount: Double, val timestamp: Long) : Event()
     data class Logout(val username: String, val timestamp: Long) : Event()
-
+}
     fun List<Event>.filterByUser(username: String): List<Event> {
         val eventsByUser = filter { it ->
             when (it) {
@@ -24,5 +28,24 @@ sealed class Event {
 
     fun processEvents(events: List<Event>, handler: (Event) -> Unit){
         events.forEach { handler(it) }
+    }
+
+fun main() {
+    val events = listOf(
+        Event.Login("alice", 1_000),
+        Event.Purchase("alice", 49.99, 1_100),
+        Event.Purchase("bob", 19.99, 1_200),
+        Event.Login("bob", 1_050),
+        Event.Purchase("alice", 15.00, 1_300),
+        Event.Logout("alice", 1_400),
+        Event.Logout("bob", 1_500)
+    )
+
+    processEvents(events) {
+        when (it) {
+            is Login -> println("[LOGIN]  ${it.username} logged in at t=${it.timestamp}")
+            is Purchase -> println("[PURCHASE]  ${it.username} spent $${it.amount} at t=${it.timestamp}")
+            is Logout -> println("[LOGOUT]  ${it.username} logged out at t=${it.timestamp}")
+        }
     }
 }
--- dam_Exer_Kotlin/src/Main.kt
+++ dam_Exer_Kotlin/src/Main.kt
@@ -1,3 +1,23 @@
+import Event.Login
+import Event.Logout
+import Event.Purchase
+
 fun main() {
-    println("Hello World!")
+    val events = listOf(
+        Event.Login("alice", 1_000),
+        Event.Purchase("alice", 49.99, 1_100),
+        Event.Purchase("bob", 19.99, 1_200),
+        Event.Login("bob", 1_050),
+        Event.Purchase("alice", 15.00, 1_300),
+        Event.Logout("alice", 1_400),
+        Event.Logout("bob", 1_500)
+    )
+
+    processEvents(events){
+        when(it) {
+            is Login -> println("[LOGIN]  ${it.username} logged in at t=${it.timestamp}")
+            is Purchase -> ("[PURCHASE]  ${it.username} spent $${it.amount} at t=${it.timestamp}")
+            is Logout -> ("[LOGOUT]  ${it.username} logged out at t=${it.timestamp}")
+        }
+    }
 }
