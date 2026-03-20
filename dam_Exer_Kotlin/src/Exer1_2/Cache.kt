a139dacc246a13dbe81d213d42473f87ea2167a2
Miguel Sedas <miguelsedas04@gmail.com>
2026-03-20 18:47:52 +0000

    Exercise 4 - Done + Add Coments

--- dam_Exer_Kotlin/src/Exer1_2/Cache.kt
+++ dam_Exer_Kotlin/src/Exer1_2/Cache.kt
@@ -3,32 +3,64 @@ package Exer1_2
 class Cache<K : Any, V : Any> {
     private val map = mutableMapOf<K, V>()
 
+    /**
+     * Função para inserir uma entrada
+     */
     fun put(key: K, value: V) {
         map[key] = value
     }
 
+    /**
+     * Função que caso a key exista, devolve o valor da respetiva Key
+     * se não existir retorna null
+     */
     fun get(key: K): V? {
         return map[key]
     }
 
+    /**
+     * Função que caso a key exista, remove a entrada da cache
+     */
     fun evict(key: K) {
         map.remove(key)
     }
 
+    /**
+     * Função que retorna o número de entradas da cache
+     * return: Int
+     */
     fun size(): Int {
         return map.size
     }
 
+    /**
+     * Função que caso a key exista retorna o respetivo valor,
+     * se não existir adiciona esse par <key, value> e retorna o valor
+     * return: map<K, V>
+     */
     fun getOrPut(key: K, defaultValue: () -> V): V{
         return map.getOrPut(key, defaultValue)
     }
 
+    /**
+     * Função que caso a key exista aplica a ação ao valor atual e retorna true,
+     * se não existir retorna false
+     * return: Boolean
+     */
     fun transform(key: K, action: (V) -> V): Boolean {
         if (map.containsKey(key)) {
             map[key] = action(map[key]!!)
             return true
         }
         return false
+    }
+
+    /**
+     * Função que retorna uma cópia do cache atual
+     * Return: map<K, V> imutável
+     */
+    fun snapshot(): Map<K, V> {
+        return map.toMap()
     }
 
 }
