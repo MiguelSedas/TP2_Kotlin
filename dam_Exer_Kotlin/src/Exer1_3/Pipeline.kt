5522ea6a51039c4440e038f7ef9f25a866fa392e
Miguel Sedas <miguelsedas04@gmail.com>
2026-03-21 20:50:31 +0000

    Exercise 1 - done

--- dam_Exer_Kotlin/src/Exer1_3/Pipeline.kt
+++ dam_Exer_Kotlin/src/Exer1_3/Pipeline.kt
@@ -14,4 +14,24 @@ class Pipeline{
     fun addStage(name : String, transform: (List<String>) -> List<String>) {
         stages.add(name to transform)
     }
+
+    /** Função que corre o input passado pelo parametro por cada stage e devolve o resultado final
+     * Parametro: input - É passado como parametro uma lista de String
+     * Return: List<String> - Retorna uma lista de string (resultado final)
+     */
+    fun execute(input: List<String>): List<String> {
+        var result = input
+        for (stage in stages){
+            result = stage.second(result)
+        }
+        return result
+    }
+
+    /**
+     * Função que printa o nome de cada stage
+     */
+    fun describe(){
+        stages.forEachIndexed { index, stage ->
+            println("${index} ${stage.first}")}
+    }
 }
